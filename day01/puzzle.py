4b8dea1696050749550db0d286354b27940c8e4e
Mob Programmer <48465586+mob-programmer@users.noreply.github.com>
2019-12-10 19:45:38 -0800

    complete calculate total fuel

--- day01/puzzle.py
+++ day01/puzzle.py
@@ -6,3 +6,7 @@ class Puzzle:
     @staticmethod
     def calculate_fuel(mass: int) -> int:
         return math.floor(mass / 3) - 2
+
+    @staticmethod
+    def calculate_total_fuel(masses: list) -> int:
+        return sum([Puzzle.calculate_fuel(mass) for mass in masses])
--- day01/test_puzzle.py
+++ day01/test_puzzle.py
@@ -11,6 +11,9 @@ class TestPuzzle(TestCase):
         self.assertEqual(654, Puzzle.calculate_fuel(1969))
         self.assertEqual(33583, Puzzle.calculate_fuel(100756))
 
+    def test_sum_of_fuel(self):
+        self.assertEqual(34241, Puzzle.calculate_total_fuel([12, 14, 1969, 100756]))
+        self.assertEqual(414, Puzzle.calculate_total_fuel([12, 1244]))
 
 # For a mass of 12, divide by 3 and round down to get 4, then subtract 2 to get 2.
 # For a mass of 14, dividing by 3 and rounding down still yields 4, so the fuel required is also 2.
